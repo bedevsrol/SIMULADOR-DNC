7c25017d0cea4b42060774980b8486a98e44b940
Bedevsrol <bdroldanv@gmail.com>
2026-08-18 10:27:59 -0500

    TEMPORAL FE

--- GENERAL/formatoExcepciones.js
+++ GENERAL/formatoExcepciones.js
@@ -1,4 +1,14 @@
-validar las lista de requiere formato excepcion
 
-cargar data basica dependiendo del mecanismo 
+
+*acomodar todos los requiere tramite en la sox de cada mecanismo que se por default sea No
+
+*que habilite el flujo del Formulario Formato excepciones
+
+*y que llene lo que viene de la base de datos y datos faltantes porque algunos vienen completos de la base informacion general de la obligacion
+
+*levantar ids de la negociacion como tal verificando con el excel y agrupar todo para hacer una sola consulta con el Formulario final
+
+*que haya boton de agregar Negociacion con excepcion y swalfire de seguro?
+
+*y que aqui se haga la consulta, una SOLA CONSULTA  
 
